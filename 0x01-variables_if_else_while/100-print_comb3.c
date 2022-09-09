c2e9b38063a399eaf0bbf2b825119d85733b0662
feyiOlufikayo <ficayosanya1@gmail.com>
2022-09-09 10:46:42 -0700

    Double combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print number from 00 - 99
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		return (0);
+
