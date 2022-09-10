733aa2da37a42570ec790fc52018fb8f8c83aa63
feyiOlufikayo <ficayosanya1@gmail.com>
2022-09-09 21:40:53 -0700

    Number combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,25 +8,22 @@
 
 int main(void)
 {
-	int i, j;
+	int h, j;
 
-	for (i = 0; i <= 99; i++)
+	for (h = 0; h <= 99; h++)
 	{
 		for (j = 0; j <= 99; j++)
 		{
-			if (i < &&i != j)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				putchar((h / 10) + '0');
+				putchar((h % 10) + '0');
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				if (i != 98 || j != 99)
+				if (h != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}
 		}
 	}
 	putchar('\n');
