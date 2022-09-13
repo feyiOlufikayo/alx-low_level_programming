6e3f2b725cbb086db822e80454ba721d90cfdaa6
feyiOlufikayo <ficayosanya1@gmail.com>
2022-09-13 09:25:04 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifindef HOLBERTON_H_INCLUDED
-#defune HOLBERTON_H_INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
 void print_alphabet(void);
@@ -16,4 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
-endif
+#endif
