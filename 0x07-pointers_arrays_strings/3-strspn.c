2be22a4631846c886390aa1d5f96a54f3898e96d
feyiOlufikayo <ficayosanya1@gmail.com>
2022-09-26 13:23:23 -0700

    Function that gets the legnth of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strspn - return legnth of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+
+unsigned int _strspn(cha *s, char *accept)
+{
+	int i = 0, j;
+	int match = 0;
+
+	while (s[i] != '\0') /*iterate through string*/
+	{
+
+		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+			if (s[i] == accept[j]) /*record & break at first match*/
+			{
+				matches++;
+				break;
+			}
+
+		if (accept[j + 1] == '\0' && s[i] != accept[j])
+			return (match); /*return if idx doesnt match*/
+	}
+
+	i++;
+}
+
+return (matches); /*Return num if all match till end*/
+}
