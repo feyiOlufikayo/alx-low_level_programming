bcdb4f328bb7289ed5785c84cd426457af2dd7d3
feyiOlufikayo <105035696+feyiOlufikayo@users.noreply.github.com>
2022-09-26 21:28:53 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,15 +6,16 @@
  * Return: number of bytes consecutively matched
  */
 
-unsigned int _strspn(cha *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0, j;
-	int match = 0;
+	int matches = 0;
 
 	while (s[i] != '\0') /*iterate through string*/
 	{
 
 		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+		{
 			if (s[i] == accept[j]) /*record & break at first match*/
 			{
 				matches++;
@@ -23,10 +24,10 @@ unsigned int _strspn(cha *s, char *accept)
 
 		if (accept[j + 1] == '\0' && s[i] != accept[j])
 			return (match); /*return if idx doesnt match*/
+		}
+		
+		i++;
 	}
-
-	i++;
-}
 
 return (matches); /*Return num if all match till end*/
 }
