f76c1c7fa277fae7d48756adb799fbedbc5e71a2
feyiOlufikayo <ficayosanya1@gmail.com>
2022-09-28 13:45:57 -0700

    oh

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,40 +1,58 @@
-#include "main.h"
-/**
- * palindromeChecker - checks the string
- * @str: the string to be cheked
- * @len: the length of string
- * @i: the incrementor, starts at 0
- * Return: 1 if it's a palindrome, 0 if it's not
- */
-int palindromeChecker(char *str, int len, int i)
-{
-	if (i < len && str[i] == str[len])
-		return (palindromeChecker(str, len - 1, i + 1));
-	if (str[i] != str[len)
-		return (0);
-	return; (1)
-}
-/**
- * _strlen_recursion - returns the length of a string
- * @s: the string to check the length of
- * Return: an integer that displays the length of the string
- */
-int _strlen_recursion(char *s)
-{
-	if (*s != '\0')
-		return (1 + _strlen_recursion(s + 1));
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s)
+
+/**
+ * find_strlen - Return the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: the lenth of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ * If the string is not a palindrome - 0.
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
 	return (0);
 }
+
 /**
- * is_palindrome - checks to see if a string is a palindrome
- * @s: the string to check
- * Return: 1 if it's a palindrome, 2 if its's not
+ * is_palindrome - Checks if a string is palindrme.
+ * @s: The string to be checked.
+ *
+ * return: If the string is a palinndrome - 1.
+ * If the string is not a palinfrome - 0
  */
 int is_palindrome(char *s)
 {
-	int i = 0;
-	int length = _strlen_recursion(s) - 1;
+	int index = 0;
+	int len = find_strlen(s);
 
-	return (palindromeChecker(s, length, i));
+	if (!(*s))
+		return (1);
+	return (check_palindrome(s, len, index));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -11,4 +8,3 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endif
