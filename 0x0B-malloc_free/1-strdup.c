03cdce50dfb52f9e16718448cb6bf9611b2ecfcf
feyiOlufikayo <ficayosanya1@gmail.com>
2022-10-04 22:38:41 -0700

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,68 +1,48 @@
 #include "main.h"
 /**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string
+ *
+ * Return: pointer of an array of chars
  */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
- * _strcpy - copy arrays
- * @src: array of elements
- * @dest: dest array
- * Return: dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest{i} = src{i};
-		i++;
-	}
-	dest[1] = '\0'
-
-		return (dest);
-}
-
-/**
- *_strdup - array for prints a sring
- * @str: array of elements
- * Return: pointer
- */
-
 char *_strdup(char *str)
+
 {
-	char *dst;
-	unsigned int size;
 
-	if (str == 0)
-	{
+	char *strout;
+
+	unsigned int i, j;
+
+
+
+	if (str == NULL)
+
 		return (NULL);
-	}
 
-	size = _strlen(str) + 1;
-	dst = (char *) malloc(size * sizeof(char));
 
-	if (dst == 0)
-	{
+
+	for (i = 0; str[i] != '\0'; i++)
+
+		;
+
+
+
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+
+
+	if (strout == NULL)
+
 		return (NULL);
-	}
 
-	_strcpy(dst, str);
-	return (dst);
+
+
+	for (j = 0; j <= i; j++)
+
+		strout[j] = str[j];
+
+
+
+	return (strout);
+
 }
