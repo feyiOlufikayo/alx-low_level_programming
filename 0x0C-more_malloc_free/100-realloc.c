ea75ec125b62fa3a2a7799c96c273dcb306acc86
feyiOlufikayo <105035696+feyiOlufikayo@users.noreply.github.com>
2022-10-06 19:04:38 +0100

    A function
    that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
  * @new_size: new size
  * Return: pointer
  */
-void *_realloc(void *ptr, unsigned int old_size, usingned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsingned int new_size)
 {
 	char *clone, *relloc;
 	unsigned int i;
